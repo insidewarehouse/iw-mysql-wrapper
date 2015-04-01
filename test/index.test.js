5c725438082cdfe09a6638265bb9eb4c56eefe5d
Dominykas Blyžė <hello@dominykas.com>
2015-04-01 20:16:49 +0300

    paramify() tests

--- test/index.test.js
+++ test/index.test.js
@@ -5,8 +5,27 @@ var expect = require("chai").expect,
 
 describe("iw-mysql-wrapper", function () {
 
-	it("should be defined", function () {
-		expect(db).to.be.a("function");
+	describe("db.paramify()", function () {
+
+		it("should return empty values/tokens when list is empty", function () {
+			var result = db.paramify([], "pfx");
+			expect(result).to.eql({
+				values: {},
+				tokens: []
+			});
+		});
+
+		it("should create a values/tokens map for the list", function () {
+			var result = db.paramify(["one", "two"], "pfx");
+			expect(result).to.eql({
+				values: {
+					"pfx0": "one",
+					"pfx1": "two"
+				},
+				tokens: [":pfx0", ":pfx1"]
+			});
+		});
+
 	});
 
 });
